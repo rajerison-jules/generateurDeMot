55873e2cafa04bd92482be400cb3f9127f8eda6f
rajerison-jules <julesrajerison@gmail.com>
2020-07-20 11:36:40 +0300

    dernier modification

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 
 var inpt = document.querySelector('.user');
 var btn = document.querySelector('.btn');
+var validation = document.querySelector('.validation')
 btn.addEventListener('click',(e)=>{
 	e.preventDefault();
 	var user= inpt.value;
@@ -11,6 +12,18 @@ function renverser(texte) {
 	var transtab = transmot.reverse();
 	var unionmot = transtab.join('');
 	return unionmot;
+}
+btn.addEventListener('click',f_valid);
+function f_valid(e){
+	if (inpt.value == "") {
+		e.preventDefault();
+		validation.innerHTML = "champ requis";
+		
+	}
+	else{
+		validation.innerHTML = "";
+		inpt.value ="";
+	}
 }
 
 
